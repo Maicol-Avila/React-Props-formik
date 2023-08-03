059040e6cb564d6605e673b7024f91455abb0a7d
Maicol Avila .F <mavila@smu.cl>
2023-08-03 14:08:49 -0400

    Seccion 8 terminada

--- src/02-components-patters/interface/interfaces.ts
+++ src/02-components-patters/interface/interfaces.ts
@@ -1,14 +1,19 @@
-import { CSSProperties, ReactElement } from "react";
+import { CSSProperties } from "react";
 
 export interface Props {
   product: Product;
-  children: ReactElement | ReactElement[];
+  children: (args: ProductCartHandlers) => JSX.Element;
   className?: string;
   styles?: CSSProperties;
   onChange?: (arg: onChangeArgs) => void;
   value?: number;
+  initialValues: InitialValues;
 }
 
+export interface InitialValues {
+  count?: number;
+  maxCount?: number;
+}
 export interface onChangeArgs {
   product: Product;
   count: number;
@@ -27,9 +32,19 @@ export interface PropsTitle {
 export interface ProductConextProps {
   Counter: number;
   increseBy: (value: number) => void;
+  maxCount?: number;
   product: Product;
 }
 
 export interface ProductIncart extends Product {
   count: number;
+}
+
+export interface ProductCartHandlers {
+  count: number;
+  isMaxCountReached?: boolean;
+  maxCount: number;
+  product: Product;
+  increseBy: (value: number) => void;
+  reset: () => void;
 }
